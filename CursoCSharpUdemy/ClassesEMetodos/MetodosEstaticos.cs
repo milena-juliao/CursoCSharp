ce0dac8c8560f5d93bceeada912c6b9eb05981c0
Milena Julião <milenadjuliao@gmail.com>
2024-05-13 10:49:16 -0300

    Métodos Estáticos.

---
+++ CursoCSharpUdemy/ClassesEMetodos/MetodosEstaticos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharpUdemy.ClassesEMetodos
+{
+    public class CalculadoraEstatica
+    {
+        public static int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplicar (int a, int b)
+        {
+            return a * b;
+        }
+    }
+
+    internal class MetodosEstaticos
+    {
+        public static void Executar()
+        {
+            //Estático
+            var resultado = CalculadoraEstatica.Somar(2, 2);
+            Console.WriteLine("Resultado: {0}", resultado);
+
+            //Instância
+            CalculadoraEstatica calc = new CalculadoraEstatica();
+            int resultado2 = calc.Multiplicar(3, 3);
+            Console.WriteLine("Resultado: {0}", resultado2);
+        }
+    }
+}
